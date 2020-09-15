09663d6292ac0b6aa8efae5af437c3ddbce1f3b8
okterakt <nicolasirbu781@gmail.com>
2020-09-15 18:24:58 +0200

    Move main loop logic from chip8 to main()

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
 use chip8::Chip8;
 use clap::{App, Arg};
-use minifb::{Scale, ScaleMode, WindowOptions};
+use minifb::{Scale, ScaleMode, Window, WindowOptions};
 use std::fs;
 use std::path::PathBuf;
+use std::time::{Duration, Instant};
 
 mod chip8;
 mod cpu;
 mod framebuffer;
 mod instr;
 mod memory;
+mod keypad;
 
 const WINDOW_WIDTH: usize = 64;
 const WINDOW_HEIGHT: usize = 32;
+
+const TIMERS_INTERVAL_MICROS: u64 = 1_000_000 / 60;
 
 fn main() {
     let matches = App::new("Chip-8")
@@ -29,7 +33,6 @@ fn main() {
         std::process::exit(1);
     });
     chip8.load_rom(&contents);
-    // chip8.run();
 
     let mut window = match minifb::Window::new(
         "Chip-8",
@@ -37,7 +40,6 @@ fn main() {
         WINDOW_HEIGHT,
         WindowOptions {
             resize: true,
-            scale: Scale::FitScreen,
             scale_mode: ScaleMode::AspectRatioStretch,
             ..WindowOptions::default()
         },
@@ -49,13 +51,31 @@ fn main() {
         }
     };
 
+    let mut last_cycle_update = Instant::now();
+    let mut last_timers_update = Instant::now();
+    let cycle_duration = Duration::from_micros((1_000_000 / 500) as u64);
+    let timers_duration = Duration::from_micros(TIMERS_INTERVAL_MICROS);
+    loop {
+        if last_cycle_update.elapsed() >= cycle_duration {
+            chip8.cpu_cycle();
+            last_cycle_update = Instant::now();
+        }
+        if last_timers_update.elapsed() >= timers_duration {
+            chip8.timers_tick();
+            update_window_buffer(&mut window, &chip8);
+            last_timers_update = Instant::now();
+        }
+    }
+}
+
+fn update_window_buffer(window: &mut Window, chip8: &Chip8) {
     let buffer: Vec<u32> = chip8
         .frame_buffer
         .buffer
         .to_vec()
         .iter()
         .map(|b| match b {
-            0 => from_u8_rgb(0x00, 0x00, 0x00),
+            0 => from_u8_rgb(0xA1, 0x3F, 0xF6),
             1 => from_u8_rgb(0xFF, 0xFF, 0xFF),
             _ => unreachable!("no such value can exist in the frame buffer"),
         })
